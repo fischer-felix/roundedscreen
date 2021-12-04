6fb88d80e2cfbf4e6465b138cab86bfc34d5dcc8
Felix Fischer <fischerfelix06@gmail.com>
2021-12-05 00:04:14 +0100

    Fix placement with latte-dock panels and click-through

--- activate.sh
+++ activate.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-#sleep 4
+sleep 4
 $SCRIPT_DIR/RoudedScreenCorners 0 &
 $SCRIPT_DIR//RoudedScreenCorners 1 &
 $SCRIPT_DIR//RoudedScreenCorners 2 &
 $SCRIPT_DIR//RoudedScreenCorners 3 &
+$SCRIPT_DIR//RoudedScreenCorners 4 &
+$SCRIPT_DIR//RoudedScreenCorners 5 &
+$SCRIPT_DIR//RoudedScreenCorners 6 &
+$SCRIPT_DIR//RoudedScreenCorners 7 &
+$SCRIPT_DIR//RoudedScreenCorners 8 &
+$SCRIPT_DIR//RoudedScreenCorners 9 &
+$SCRIPT_DIR//RoudedScreenCorners 10 &
+$SCRIPT_DIR//RoudedScreenCorners 11 &
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,29 +12,61 @@ MainWindow::MainWindow(QMainWindow *parent, unsigned short corner)
     setWindowFlag(Qt::Tool);
     setWindowFlag(Qt::FramelessWindowHint);
     setWindowFlag(Qt::WindowStaysOnTopHint);
-    setAttribute(Qt::WA_TranslucentBackground);
+    setWindowFlag(Qt::X11BypassWindowManagerHint);
+    setAttribute(Qt::WA_TranslucentBackground, true);
+    setWindowFlag(Qt::WindowTransparentForInput, true);
 
     QPalette pal = palette();
     pal.setColor(QPalette::Window,QColor(0,0,0,0));
     setPalette(pal);
 
     if (corner == 0) {
-        setWindowTitle("Left_Top");
-        setGeometry(1920,0,24,24);
+        setWindowTitle("Left_Top_1");
+        setGeometry(0,0,24,24);
     } else if (corner == 1) {
-        setWindowTitle("Right_Top");
-        setGeometry(1920+1920-24,0,24,24);
+        setWindowTitle("Right_Top_1");
+        setGeometry(QApplication::primaryScreen()->size().width()-24,0,24,24);
     } else if (corner == 2) {
-        setWindowTitle("Left_Bottom");
-        setGeometry(1920,1080-24,24,24);
+        setWindowTitle("Left_Bottom_1");
+        setGeometry(0,QApplication::primaryScreen()->size().height()-24,24,24);
     } else if (corner == 3) {
-        setWindowTitle("Right_Bottom");
-        setGeometry(1920+1920-24,1080-24,24,24);
+        setWindowTitle("Right_Bottom_1");
+        setGeometry(QApplication::primaryScreen()->size().width()-24,QApplication::primaryScreen()->size().height()-24,24,24);
+    } 
+
+    else if (corner == 4) {
+        setWindowTitle("Left_Top_2");
+        setGeometry(QApplication::primaryScreen()->size().width()+0,0,24,24);
+    } else if (corner == 5) {
+        setWindowTitle("Right_Top_2");
+        setGeometry(QApplication::primaryScreen()->size().width()+QApplication::primaryScreen()->size().width()-24,0,24,24);
+    } else if (corner == 6) {
+        setWindowTitle("Left_Bottom_2");
+        setGeometry(QApplication::primaryScreen()->size().width()+0,QApplication::primaryScreen()->size().height()-24,24,24);
+    } else if (corner == 7) {
+        setWindowTitle("Right_Bottom_2");
+        setGeometry(QApplication::primaryScreen()->size().width()+QApplication::primaryScreen()->size().width()-24,QApplication::primaryScreen()->size().height()-24,24,24);
+    } 
+
+    else if (corner == 8) {
+        setWindowTitle("Left_Top_3");
+        setGeometry(3840,0,24,24);
+    } else if (corner == 9) {
+        setWindowTitle("Right_Top_3");
+        setGeometry(4866,0,24,24);
+    } else if (corner == 10) {
+        setWindowTitle("Left_Bottom_3");
+        setGeometry(3840,1656,24,24);
+    } else if (corner == 11) {
+        setWindowTitle("Right_Bottom_3");
+        setGeometry(4866,1656,24,24);
     }
 }
 
 MainWindow::~MainWindow()
 {
+    setAttribute(Qt::WA_TranslucentBackground, true);
+    setAttribute(Qt::WA_TransparentForMouseEvents);
 }
 
 void MainWindow::paintEvent(QPaintEvent *event)
@@ -43,6 +75,8 @@ void MainWindow::paintEvent(QPaintEvent *event)
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing, true);
 
+    setAttribute(Qt::WA_TranslucentBackground, true);
+    setAttribute(Qt::WA_TransparentForMouseEvents);
     painter.setBrush(QBrush(QColor(0,0,0,255)));
     painter.setPen(QPen(QColor(0,0,0,255),10));
 
@@ -50,5 +84,13 @@ void MainWindow::paintEvent(QPaintEvent *event)
     if (corner == 1) painter.drawArc(-5,-5,width()+10,height()+10,0,1200);
     if (corner == 2) painter.drawArc(-5,-5,width()+10,height()+10,2900,1200);
     if (corner == 3) painter.drawArc(-5,-5,width()+10,height()+10,-1300,1200);
+    if (corner == 4) painter.drawArc(-5,-5,width()+10,height()+10,1600,1000);
+    if (corner == 5) painter.drawArc(-5,-5,width()+10,height()+10,0,1200);
+    if (corner == 6) painter.drawArc(-5,-5,width()+10,height()+10,2900,1200);
+    if (corner == 7) painter.drawArc(-5,-5,width()+10,height()+10,-1300,1200);
+    if (corner == 8) painter.drawArc(-5,-5,width()+10,height()+10,1600,1000);
+    if (corner == 9) painter.drawArc(-5,-5,width()+10,height()+10,0,1200);
+    if (corner == 10) painter.drawArc(-5,-5,width()+10,height()+10,2900,1200);
+    if (corner == 11) painter.drawArc(-5,-5,width()+10,height()+10,-1300,1200);
 }
 
